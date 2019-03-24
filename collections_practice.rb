d3cb7a279f3171abc66172a7621596960939695f
Samuel T <ssamt1142@gmail.com>
2019-03-24 02:18:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+require "pry"
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |num1,num2|
+    num2 <=> num1
+  end
+end
+
+def sort_array_char_count(array)
+    array.sort_by! {|string| string.size}
+end
+
+def swap_elements(array)
+  store = []
+  store << array.delete_at(1)
+  array += store
+end
+
+def reverse_array(array)
+  first = []
+  last = []
+  first << array.shift
+  last << array.pop
+  array += first
+  array.unshift(last)
+  array.flatten
+end
+
+def kesha_maker(array)
+  array.collect do |x|
+    #x[2] = "$"
+    #x
+    x.insert(2, "$")
+    x.delete!(x[3])
+    x
+  end
+end
+
+def find_a(array)
+  a = []
+  array.each do |x|
+    binding.pry
+    #a << x.start_with?(a) == true
+  end
+  a
+end
+
+def sum_array(array)
+  array.sum_array
+end
+
+def add_s(array)
+end
